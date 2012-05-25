f01e08f71057550c042b4c18d7e1885d294a7e69
maxim.karban@gmail.com <maxim.karban@gmail.com@8db61ce9-5e8f-94c8-fa6b-1d1c8bcca7d0>
2012-05-25 17:36:05 +0000

    EEPROM features reworked

--- translations/cz.h
+++ translations/cz.h
@@ -313,8 +313,6 @@
 #define TR_MULTIPLIER   "N\200sobi\201"
 #define TR_CAL          "Kal."
 #define TR_EEPROMV      "EEPROM v"
-#define TR_EEPROM_VAR_V4  "V4"
-#define TR_EEPROM_VAR_ARM "ARM"
 #define TR_VTRIM        "Trim- +"
 #define TR_BG           "BG:"
 #define TR_MENUTOSTART  " [MENU] = START"
--- translations/de.h
+++ translations/de.h
@@ -308,8 +308,6 @@
 #define TR_MULTIPLIER                   "Multiplikator"                                   
 #define TR_CAL                          "Kal."                                            
 #define TR_EEPROMV                      "EEPROM v"                                        
-#define TR_EEPROM_VAR_V4                "V4"
-#define TR_EEPROM_VAR_ARM               "ARM"
 #define TR_VTRIM                        "Trim- +"                                         
 #define TR_BG                           "BG:"                                             
 #define TR_MENUTOSTART                  "[MENU] > Starten"                                
